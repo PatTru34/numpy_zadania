560d3238d687cabbe0753d73914193015dc8e893
PatTru34 <PatTru34>
2021-04-14 10:26:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+# Zad 1
+# a=np.arange(4,4*20+1,4)
+# print(a)
+
+# Zad 2
+
+# b=np.arange(20,dtype='float')
+# p=b.astype('int32')
+# print(b)
+# print(p)
+
+# Zad 3
+
+# def fun(n):
+#     return np.logspace(1,n*n,num=n*n,base=2,dtype='int64').reshape(n,n)
+#
+# print(fun(3))
+
+# Zad 4
+# def generuj(i,n):
+#     return np.logspace(1,n,num=n,base=i,dtype='int64')
+# print(generuj(3,4))
+
+# Zad 5
+# def wektor(l):
+#
+#     odwrot= np.arange(l-1,-1,-1)
+#     return np.diag(odwrot,k=2)
+#
+# print(wektor(4))
+
+
